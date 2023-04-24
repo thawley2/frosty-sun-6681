5e6b24d6dfc69fab59a0b3b434e88a1df98d8e39
Thomas Hawley <thawley22@turing.edu>
2023-04-24 09:50:49 -0600

    test: Add #all_patients test

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -13,12 +13,29 @@ RSpec.describe Doctor do
   let!(:doctor2) {hospital2.doctors.create!(name: 'Jerry', specialty: 'Oncology', university: 'UCO')}
   let!(:doctor3) {hospital3.doctors.create!(name: 'Rigby', specialty: 'Oncology', university: 'UCO')}
 
+  let!(:patient1) {Patient.create!(name: 'Janey', age: 30)}
+  let!(:patient2) {Patient.create!(name: 'Ringo', age: 16)}
+  let!(:patient3) {Patient.create!(name: 'Starsky', age: 1)}
+
+  let!(:link1) {DoctorPatient.create!(doctor: doctor1, patient: patient1)}
+  let!(:link2) {DoctorPatient.create!(doctor: doctor1, patient: patient2)}
+  let!(:link3) {DoctorPatient.create!(doctor: doctor1, patient: patient3)}
+  let!(:link4) {DoctorPatient.create!(doctor: doctor3, patient: patient3)}
+
   describe 'instance methods' do
     describe '#hospital_name' do
       it 'returns the name of the hospital the doctor works for' do
         expect(doctor1.hospital_name).to eq('Saint Anthonys')
         expect(doctor2.hospital_name).to eq('Main st clinic')
         expect(doctor3.hospital_name).to eq('South st clinic')
+      end
+    end
+
+    describe '#all_patients' do
+      it 'returns an array of all patient names for the doctor' do
+        expect(doctor1.all_patients).to eq(['Janey', 'Ringo', 'Starsky'])
+        expect(doctor2.all_patients).to eq([])
+        expect(doctor3.all_patients).to eq(['Starsky'])
       end
     end
   end
