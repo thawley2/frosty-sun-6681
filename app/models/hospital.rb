3e6bc1c5508787a139724e4b3707b0c47a7b6d19
Thomas Hawley <thawley22@turing.edu>
2023-04-24 11:49:15 -0600

    feat: Add #doctors_patient_count method to hopital model

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,12 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+  has_many :doctor_patients, through: :doctors
+  has_many :patients, through: :doctor_patients
+
+  def doctors_patient_count
+    doctors.joins(:patients)
+    .select('doctors.*, count(patients) as num_patients')
+    .group(:id)
+    .order(num_patients: :desc)
+  end
 end
