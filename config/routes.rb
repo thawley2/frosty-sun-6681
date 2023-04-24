be1c56d328e8e4ca25f971b50532f3b42e9ff002
Thomas Hawley <thawley22@turing.edu>
2023-04-24 11:47:53 -0600

    feat: Add route to hospitals show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :doctors, only: :show
   resources :doctor_patients, only: :destroy
   resources :patients, only: :index
+  resources :hospitals, only: :show
 end
